b6371faf3116e6234385828bf446ec05c1cd3bcb
Tanya Kashyap <kashyaptanya105@gmail.com>
2022-12-28 00:51:59 +0530

    update

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,50 +1,49 @@
 import { useEffect, useState } from "react"
 import { db } from "../firebase"
 import Signout from "./Signout"
-import { doc, getDoc,setDoc ,collection } from "firebase/firestore";
+import { doc, getDoc, onSnapshot  } from "firebase/firestore";
+// import { doc, getDoc,setDoc ,collection } from "firebase/firestore";
 
 function Chat() {
-    // const [message,setMessage] = useState([])
-    const mesRef = collection(db, "message");
-    
-    setDoc(doc(mesRef,"1"), {
-        text:"tanyaa",})
-    
-
-    const docRef = doc(db, "messaage");
-    const docSnap = getDoc(docRef);
-
-    if (docSnap.exists()) {
-        console.log("Document data:", docSnap.data());
-    } else {
-         // doc.data() will be undefined in this case
-        console.log("No such document!");
-    }
-
+    const [message,setMessage] = useState([])
+   
     useEffect(() => {
-
-        // db.collection("message").onSnapshot((snapshot) => {
-        //     console.log("byyuh")
-        //     setMessage(
-        //         snapshot.docs.map((doc) => ({
-        //             id: doc.id,
-        //             data: doc.data(),
-        //         }))
-        //     );   
-        // });
-        // console.log({ setMessage });
+        fetchText()
     }, [])
+
+    const fetchText = async()=>{
+        // const querySnapshot = await getDocs(collection(db, "message"));
+        // const docRef = doc(db, "message", "Wih6fOensc9SA1TnIro7");        
+        // const docSnap = await getDoc(docRef);
+
+        // if (docSnap.exists()) {
+        //     console.log("Document data:", docSnap.data());
+        //   } else {
+        //     // doc.data() will be undefined in this case
+        //     console.log("No such document!");
+        //   }
+
+        onSnapshot(doc(db, "message", "Wih6fOensc9SA1TnIro7"), (doc) => {
+            console.log("Current data: ", doc.data());
+        });
+        // setMessage(
+        //     querySnapshot.docs.map((doc) => ({
+        //         id: doc.id,
+        //        data: doc           
+        //     }))    
+        // )
+    }
 
     return (
         <>
             <Signout />
-            {/* {
+            {
             message.map(({id,data})=>{
                 <div key={id} >
                     <p>{data}</p>
                 </div>
             })
-        } */}
+        }
         </>
     )
 }
